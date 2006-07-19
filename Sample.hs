c51272336cc32feb9d14f22f5426c5567a0db750
Neil Mitchell <ndmitchell@gmail.com>
2006-07-19 19:38:31 +0000

    Start to implement toolbar buttons in the sample

--- Sample.hs
+++ Sample.hs
@@ -8,9 +8,10 @@ data Gui = Gui {
     window :: Window,
     txt :: TextBox,
     sb :: StatusBar,
---    open :: ToolButton,
---    save :: ToolButton,
---    close :: ToolButton,
+    new :: ToolButton,
+    open :: ToolButton,
+    save :: ToolButton,
+    close :: ToolButton,
     
     document :: Var Bool,
     modified :: Var Bool,
@@ -24,23 +25,35 @@ main = do
     window <- getWindow "sample.glade" "wndMain"
     txt <- getTextBox window "txt"
     sb <- getStatusBar window "sb"
+    new <- getToolButton window "tbNew"
+    open <- getToolButton window "tbOpen"
+    save <- getToolButton window "tbSave"
+    close <- getToolButton window "tbClose"
     
     e <- newEventName "Sample.test"
     e += putStrLn "event fired"
     raise e
     
+    -- is a document open
     document <- newVar False
+    
+    -- is a document modified
     modified <- newVar False
+    
+    -- the filename of the document, if its been saved
     filename <- newVar Nothing
     
-    let gui = Gui{window=window, sb=sb, txt=txt,
-                  document=document, modified=modified, filename=filename}
+--    let gui = Gui{window=window, sb=sb, txt=txt,
+--                  document=document, modified=modified, filename=filename}
     
     
     -- window!text =<= txt!text
     -- txt!text -< "hi everyone :)"
     
     txt!enabled =<= document
+    new!enabled =< with1 document not 
+    close!enabled =<= document
+    save!enabled =< with2 document modified (&&)
     
     
     sb!text =< with1 (txt!text) (\x ->
