6d1d03de1a6e0709b0248d8cc31453e0bc565cd9
mail@joachim-breitner.de <unknown>
2006-08-15 13:16:55 +0000

    Minor cosmetics, take two

--- PropLang/Variable.hs
+++ PropLang/Variable.hs
@@ -9,6 +9,7 @@ module PropLang.Variable(
 import PropLang.Event
 import PropLang.Value
 import Data.IORef
+import Monad
 
 infixr 1  =<, -<, =<=
 
@@ -35,21 +36,21 @@ newVarWithName name f = do
 
 
 getVar :: Var a -> IO a
-getVar (Var a b c) = valGet a
+getVar (Var val _ _) = valGet val
 
 
 instance Eventer (Var a) where
-    event (Var a b c) = b
+    event (Var _ evt _) = evt
 
 
 (-<) :: Var a -> a -> IO ()
-(-<) (Var val b source) x = do
+(-<) (Var val evt source) x = do
     writeIORef source []
     (valSet val) x
 
 
 (=<=) :: Var a -> Var a -> IO ()
-a =<= b = a =< with1 b id
+var1 =<= var2 = var1 =< with1 var2 id
 
 
 with = with1
@@ -58,25 +59,19 @@ with1 :: Var a -> (a -> x) -> Var x -> IO ()
 with1 varFrom1 f var =
         withN [event varFrom1] g var
     where
-        g = do v <- getVar varFrom1
-               return $ f v
+        g = liftM f $ getVar varFrom1
 
 with2 :: Var a -> Var b -> (a -> b -> x) -> Var x -> IO ()
 with2 varFrom1 varFrom2 f var =
         withN [event varFrom1, event varFrom2] g var
     where
-        g = do v1 <- getVar varFrom1
-               v2 <- getVar varFrom2
-               return $ f v1 v2
+        g = liftM2 f (getVar varFrom1) (getVar varFrom2) 
 
 with3 :: Var a -> Var b -> Var c -> (a -> b -> c -> x) -> Var x -> IO ()
 with3 varFrom1 varFrom2 varFrom3 f var =
         withN [event varFrom1, event varFrom2, event varFrom3] g var
     where
-        g = do v1 <- getVar varFrom1
-               v2 <- getVar varFrom2
-               v3 <- getVar varFrom3
-               return $ f v1 v2 v3
+        g = liftM3 f (getVar varFrom1) (getVar varFrom2) (getVar varFrom3)
 
 
 withN :: [Event] -> IO x -> Var x -> IO ()
